ef9a81ee5fcfd0f3b628af6e76d16218ff6dff83
DanielRasho <ray22933@uvg.edu.gt>
2024-11-11 18:20:43 -0600

    feat: add nix flake

--- flake.nix
+++ flake.nix
@@ -3,18 +3,33 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
 
-  outputs = { self, nixpkgs }: {
-    devShells.default = nixpkgs.lib.mkShell {
-      packages = with nixpkgs.pkgs; [
-        ghc          # The Glasgow Haskell Compiler
-        cabal-install # Cabal build tool
-      ];
+  outputs = { self, nixpkgs }: 
+    let 
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in 
+    {
+      devShells = forAllSystems (system: 
+        let 
+          pkgs = import nixpkgs {
+            inherit system; 
+            config.allowUnfree = true;
+          };
+        in 
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              (haskell.compiler.ghc94)           # The Glasgow Haskell Compiler
+              cabal-install # Cabal build tool
+            ];
 
-      shellHook = ''
-        echo "Welcome to your Haskell development environment!"
-        echo "GHC version: $(ghc --version)"
-        echo "Cabal version: $(cabal --version)"
-      '';
+            shellHook = ''
+              echo "Welcome to your Haskell development environment!"
+              echo "GHC version: $(ghc --version)"
+              echo "Cabal version: $(cabal --version)"
+            '';
+          };
+        }
+      );
     };
-  };
 }
